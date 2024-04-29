4371bd1f683f95967c6de770c3402c6c778843f7
Arman Bhatia <arman.bhatia.1407@gmail.com>
2024-04-28 20:19:19 -0400

    deleted the react site.

--- my-react-app/src/components/Header/Overlay.jsx
+++
@@ -1,8 +0,0 @@
-export default function Overlay() {
-  return (
-    <div
-      data-attribute="Overlay"
-      className="z-20 absolute bg-zinc-900 bg-opacity-70 min-w-full h-full min-h-screen overlay"
-    ></div>
-  );
-}
